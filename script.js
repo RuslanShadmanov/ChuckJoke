01ef238d9820df1ad70ae126772803997e391d68
Ruslan <shadmanov.ruslan@gmail.com>
2023-12-16 11:02:36 -0800

    add dropbox list

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const button = document.querySelector("button")
 const jokeDiv = document.querySelector (".joke")
+const selectCategory = document.querySelector("select")
+// const listCategory = document.querySelector(".listCategory")
+
 
 
 
 const getRandomJoke = async()=>{
     const res = await fetch ("https://api.chucknorris.io/jokes/random")
+   
     
     const joke = await res.json();
-    jokeDiv.innerText= joke.value
+
+    console.log (joke)
+
+    jokeDiv.innerText= joke.value;
+   
     
 }
+
+
+
+const chooseCategory = async()=>{
+    const res =await fetch("https://api.chucknorris.io/jokes/categories")
+    const category = await res.json();
+    // for(let el of category){
+    //     const option = document.createElement("option")
+    //     option.innerText = el;
+    //     option.value=el;    
+    //     selectCategory.appendChild(option)
+    // }
+    category.forEach ((category)=>{
+        const option = document.createElement("option")
+        option.innerText = category;
+        option.value=category;    
+        selectCategory.appendChild(option)
+    })
+}
 button.addEventListener("click", getRandomJoke);
+document.addEventListener("DOMContentLoaded",chooseCategory)
+
+
+
+
